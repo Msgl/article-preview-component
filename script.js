a1eaa26181f7763825db0d984b549ddc52574528
Msg89 <mariasglynou@gmail.com>
2020-12-17 15:21:57 +0200

    SVG functionality

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const  shareBtn = document.querySelector('.share-btn');
 const  shareContainer = document.querySelector('.share-container');
+const  svgSharePath = document.querySelector('.svg-share-path');
 
 shareBtn.addEventListener('click',function() {
-    if(shareContainer.style.display === 'flex') {
+    if (shareContainer.style.display === 'flex') {
         shareContainer.style.display = 'none';
-    }   else {
+        shareBtn.style.backgroundColor = '';
+        svgSharePath.style.fill = '';
+       
+    } else {
         shareContainer.style.display = 'flex';
+        shareBtn.style.backgroundColor = 'hsl(214, 17%, 51%)';
+        svgSharePath.style.fill = 'white';
     }
 })
